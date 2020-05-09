1735aac59203cc2527fbcce4ea679d43a9177152
TERP <terposterp@yandex.com>
2020-05-09 18:45:58 -0400

    5-9-2020
    Sorry I did not upload the source codes in time. Here are the source codes.

---
+++ C++_With_Cevelop_15 - Global and Local.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+string sentence = "I am a variable!!";
+void print()
+{
+	cout << sentence << endl;
+}
+
+int main(){
+	string word = "Local";
+
+	cout << word << endl;
+
+	return 0;
+}
